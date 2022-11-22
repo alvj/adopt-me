c3037c840ff20e4113b5cf420bcb8af6b9834cf3
Álvaro Pérez <alvarojperezperez@gmail.com>
2022-11-22 10:37:56 +0100

    Include jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Caramel",
-      animal: "Dog",
-      breed: "German Shepherd",
-    }),
-    React.createElement(Pet, {
-      name: "Mowgli",
-      animal: "Dog",
-      breed: "Mix",
-    }),
-    React.createElement(Pet, {
-      name: "Koda",
-      animal: "Dog",
-      breed: "Samoyed",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Caramel" animal="Dog" breed="German Shepherd" />
+      <Pet name="Koda" animal="Dog" breed="Samoyed" />
+      <Pet name="Mowgli" animal="Dog" breed="Mix" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
